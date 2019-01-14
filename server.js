3c4fb076f6ae42ed5b8a5f81f6179855afc95c66
Daniele Angelucci <angelucci.dnl@gmail.com>
2019-01-14 13:07:24 +0100

    Send write request to influxDB

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var request = require('request');
 var storeTimestamp = function(req, res) {
 
 
-request('http://www.google.com', function (error, response, body) {
+request.post({
+	url: 'http://localhost:8086/write?db=mydb',
+	body: "cpu_load_short,host=server0X,region=us-west value=1"
+	}, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 	console.log("DONE");
