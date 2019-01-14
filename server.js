cb60287c259395968cc3a7248f38da264c98b2cb
Daniele Angelucci <angelucci.dnl@gmail.com>
2019-01-14 12:57:39 +0100

    added a simple client request (GET)

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var http = require('http');
+var request = require('request');
 
-var instructionsNewVisitor = function(req, res) {
+var storeTimestamp = function(req, res) {
+
+
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	console.log("DONE");
+});
+
+
 res.writeHead(200);
-res.end('Hi everybody!');
+res.end('Stored!');
 }
 
-var server = http.createServer(instructionsNewVisitor);
+var server = http.createServer(storeTimestamp);
 
 server.listen(8085);
