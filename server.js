b7e9320bbf858e260aad1e32725a24f4dc5a0b8a
Daniele Angelucci <angelucci.dnl@gmail.com>
2019-01-14 14:04:39 +0100

    Added logs.

--- server.js
+++ server.js
@@ -6,16 +6,26 @@ var storeTimestamp = function(req, res) {
 
 request.post({
 	url: 'http://localhost:8086/write?db=mydb',
-	body: "cpu_load_short,host=server0X,region=us-west value=1"
+	//body: "cpu_load_short,host=server0X,region=us-west value=1"
+	body: 'eventlog,host=server0X,region=us-west value="Received a POST request event."'
+
 	}, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	console.log("DONE");
+		if(response && response.statusCode){
+		  	console.log('error:', error); // Print the error if one occurred
+		  	console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+			console.log('body:', response.body);
+
+			if(response.statusCode===204){
+				res.writeHead(200);
+				res.end('Event stored successfully!');
+			}
+			else{
+				res.writeHead(400);
+				res.end();
+			}
+		}
+
 });
-
-
-res.writeHead(200);
-res.end('Stored!');
 }
 
 var server = http.createServer(storeTimestamp);
