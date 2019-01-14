cb16e894be0f2a61530cecfb18979502a6b0a3c7
Daniele Angelucci <angelucci.dnl@gmail.com>
2019-01-14 14:33:40 +0100

    Forced POST request.

--- server.js
+++ server.js
@@ -3,29 +3,36 @@ var request = require('request');
 
 var storeTimestamp = function(req, res) {
 
+    if (req.method == 'POST') {
 
-request.post({
-	url: 'http://localhost:8086/write?db=mydb',
-	//body: "cpu_load_short,host=server0X,region=us-west value=1"
-	body: 'eventlog,host=server0X,region=us-west value="Received a POST request event."'
+		request.post({
+			url: 'http://localhost:8086/write?db=mydb',
+			//body: "cpu_load_short,host=server0X,region=us-west value=1"
+			body: 'eventlog,host=server0X,region=us-west value="Received a POST request event."'
 
-	}, function (error, response, body) {
-		if(response && response.statusCode){
-		  	console.log('error:', error); // Print the error if one occurred
-		  	console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
-			console.log('body:', response.body);
+			}, function (error, response, body) {
+				if(response && response.statusCode){
+				  	console.log('error:', error); // Print the error if one occurred
+				  	console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+					console.log('body:', response.body);
 
-			if(response.statusCode===204){
-				res.writeHead(200);
-				res.end('Event stored successfully!');
-			}
-			else{
-				res.writeHead(400);
-				res.end();
-			}
-		}
+					if(response.statusCode===204){
+						res.writeHead(200);
+						res.end('Event stored successfully!');
+					}
+					else{
+						res.writeHead(400);
+						res.end();
+					}
+				}
 
-});
+		});
+	}
+	else{
+		res.writeHead(405,  {'Allow': 'POST'});
+		res.end();
+	}
+
 }
 
 var server = http.createServer(storeTimestamp);
